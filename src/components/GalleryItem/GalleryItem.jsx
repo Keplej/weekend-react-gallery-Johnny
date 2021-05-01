1a40ac5745403290932b953799c99edf4d52ba16
Johnny Keple <Johnny.Keple@gmail.com>
2021-05-01 11:19:26 -0500

    Got the button to respond in the console

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,16 +3,13 @@
 // Trying to get images from the image folder without trying to list them all out
 // Maybe by trying to grab the id?
 function GalleryItem(props) {
-
-
-
     const image = props.image;
-    const item = props.like;
+    
     return (
         <div>
             <img src={image.path} alt={image.description} height="300px"/>
-            {/* <button onClick { () => {likeCount())}}></button> */}
-            <button>Like!</button>
+            <button onClick ={ () => props.likeCount(props.image.id)} id={props.image.id}>Like!</button>
+            {/* <p {props.image.}></p> */}
         </div>
     )
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -5,7 +5,7 @@ function GalleryList(props) {
     return (
         <div>
             {props.galleryList.map (image => 
-                (<GalleryItem key={image.id} image={image} likeCount={likeCount}/>)
+                (<GalleryItem key={image.id} image={image} likeCount={props.likeCount}/>)
                 //Trying to add in a like counter. Starting here to have to linked up after the 
                 //code in gallery item is added in
             )}
